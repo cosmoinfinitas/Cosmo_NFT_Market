42e67fcaf29470a7d7447a276cd3ed36ca2ae926
cosmoinfinitas <91360858+cosmoinfinitas@users.noreply.github.com>
2022-02-28 16:57:35 +0530

    editeed

--- pages/createitem.js
+++ pages/createitem.js
@@ -160,9 +160,9 @@ function createitem() {
                       fill="currentColor"
                     >
                       <path
-                        fillRule="evenodd"
+                        fill-rule="evenodd"
                         d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z"
-                        clipRule="evenodd"
+                        clip-rule="evenodd"
                       />
                     </svg>
                     <p className="pt-1 text-sm tracking-wider text-blue-400 group-hover:text-blue-600">
